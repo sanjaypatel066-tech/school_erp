7a051ba70c60ad7049b13a34f611ee2f9b2fa12a
sanjaypatel066-tech <sanjay.patel066@gmail.com>
2026-09-04 19:38:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,6 @@ else:
     elif menu == "🤖 AI અહેવાલ":
         st.header("🤖 સ્માર્ટ AI અહેવાલ લેખક")
         genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-        
         topic = st.text_area("અહેવાલની ટૂંકી વિગત લખો:", placeholder="દા.ત. વિજ્ઞાન મેળો, 50 પ્રોજેક્ટ...")
         
         if st.button("✨ અહેવાલ બનાવો"):
